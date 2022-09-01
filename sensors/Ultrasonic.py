68e81b9e26f9dff9a1050dc18ee37e6ca0855a50
toomas-siim <siim.teresk@veebiteenused.ee>
2022-09-02 01:09:59 +0300

    #6, ultrasonic sensor logic

--- sensors/Ultrasonic.py
+++ sensors/Ultrasonic.py
@@ -1,7 +1,24 @@
-class Ultrasonic(Sensor):
-    def handle():
-        print("Handling ultrasonic sensor.")
+import time
 
-    # Calculate altitude
-    def getAltitude():
-        return 0
+class Ultrasonic(Sensor):
+    waitingForSignal = False
+    signalStart = None
+    latestDistance = 0
+    # @TODO: load pins dynamically.
+    readPin = 0
+    writePin = 1
+
+    def handle(self, coreHandle):
+        if self.waitingForSignal is False and time.time() - self.signalStart >= 1:
+            self.waitingForSignal = True
+            self.signalStart = time.time()
+            self.pinSystem.setPinType(self.writePin, gpio.OUTPUT)
+            self.pinSystem.setPinType(self.readPin, gpio.INPUT)
+        elif self.waitingForSignal is True:
+            if coreHandle.pinSystem.getPinInput(self.readPin) is 1:
+                self.waitingForSignal = False
+                self.latestDistance = ((time.time() - self.signalStart) * 34300) / 2
+
+    # Fetch distance
+    def getDistance(self):
+        return self.latestDistance
