79afc7982e02a6be307c368379fdbabb9feecb0a
toomas-siim <siim.teresk@veebiteenused.ee>
2022-09-05 00:31:05 +0300

    #2, scripting system

--- scripts/circle.py
+++ scripts/circle.py
@@ -23,7 +23,7 @@ class Circle:
             self.calcRotationPoints()
             self.currentPoint = 0
         if len(self.rotationPoints) > 0:
-            distance = Navigation.distanceFrom(self.rotationPoints[self.currentPoint].lat, self.rotationPoints[self.currentPoint].lon)
+            distance = Navigation.distanceFrom(self.rotationPoints[self.currentPoint].lat, self.rotationPoints[self.currentPoint].lon) / 1000
             if distance < 5:
                 if len(self.rotationPoints) <= self.currentPoint:
                     self.currentPoint = 0
