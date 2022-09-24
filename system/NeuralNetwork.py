46d7fafdd50462b2ffadb4c03414de7b41d9bd34
toomas-siim <siim.teresk@veebiteenused.ee>
2022-09-24 03:33:58 +0300

    #29, neural net

--- system/NeuralNetwork.py
+++ system/NeuralNetwork.py
@@ -1,81 +1,102 @@
-import os
-import glob
+import matplotlib.pyplot as plt
 import numpy as np
-import cv2
-from sklearn.utils import shuffle
+import PIL
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+import pathlib
 
 NeuralNetwork:
-    def load_train(train_path, image_size, classes):
-        images = []
-        labels = []
-        ids = []
-        cls = []
-        print('Loading {} files (Index: {})'.format(fld, index))
-        path = os.path.join(train_path, fld, '*.' + dataType + '.png')
-        files = glob.glob(path)
-        for fl in files:
-            image = cv2.imread(fl)
-            image = cv2.resize(image, (image_size, image_size), cv2.INTER_LINEAR)
-            images.append(image)
-            label = np.zeros(len(classes))
-            label[index] = 1.0
-            labels.append(label)
-            flbase = os.path.basename(fl).splt('.')
-            ids.append(flbase[0])
-        images = np.array(images)
-        labels = np.array(labels)
-        ids = np.array(ids)
+    coreHandle = None
+    model = None
+    classNames = None
 
-        return images, labels, ids
+    def init(self, coreHandle):
+        self.coreHandle = coreHandle
+        dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz" # @TODO: Change
+        data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
+        data_dir = pathlib.Path(data_dir)
 
+    def predict(self):
+        sunflower_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg" # @TODO: Change
+        sunflower_path = tf.keras.utils.get_file('Red_sunflower', origin=sunflower_url)
 
-    def load_test(test_path, image_size):
-        path = os.path.join(test_path, '*g')
-        files = sorted(glob.glob(path))
+        img = tf.keras.utils.load_img(
+            sunflower_path, target_size=(img_height, img_width)
+        )
+        img_array = tf.keras.utils.img_to_array(img)
+        img_array = tf.expand_dims(img_array, 0) # Create a batch
 
-        X_test = []
-        X_test_id = []
-        print("Reading test images")
-        for fl in files:
-            flbase = os.path.basename(fl)
-            img = cv2.imread(fl)
-            img = cv2.resize(img, (image_size, image_size), cv2.INTER_LINEAR)
-            X_test.append(img)
-            X_test_id.append(flbase)
-
-        ### because we're not creating a DataSet object for the test images, normalization happens here
-        X_test = np.array(X_test, dtype=np.uint8)
-        X_test = X_test.astype('float32')
-        X_test = X_test / 255
-
-        return X_test, X_test_id
-
-    def read_train_sets(train_path, image_size, classes, validation_size=0):
-        class DataSets(object):
-            pass
-
-        data_sets = DataSets()
-
-        images, labels, ids = load_train(train_path, image_size, classes)
-        images, labels, ids = shuffle(images, labels, ids)  # shuffle the data
-
-        if isinstance(validation_size, float):
-            validation_size = int(validation_size * images.shape[0])
-
-        validation_images = images[:validation_size]
-        validation_labels = labels[:validation_size]
-        validation_ids = ids[:validation_size]
-
-        train_images = images[validation_size:]
-        train_labels = labels[validation_size:]
-        train_ids = ids[validation_size:]
-
-        data_sets.train = DataSet(train_images, train_labels, train_ids)
-        data_sets.valid = DataSet(validation_images, validation_labels, validation_ids)
-
-        return data_sets
+        predictions = self.model.predict(img_array)
+        score = tf.nn.softmax(predictions[0])
+        print(
+            "This image most likely belongs to {} with a {:.2f} percent confidence."
+            .format(self.class_names[np.argmax(score)], 100 * np.max(score))
+        )
 
 
-    def read_test_set(test_path, image_size):
-        images, ids = load_test(test_path, image_size)
-        return images, ids
+
+    def train(self):
+        epochs=10
+        return model.fit(
+          train_ds,
+          validation_data=val_ds,
+          epochs=epochs
+        )
+
+
+    def model(self):
+        num_classes = len(self.class_names)
+
+        self.model = Sequential([
+          layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+          layers.Conv2D(16, 3, padding='same', activation='relu'),
+          layers.MaxPooling2D(),
+          layers.Conv2D(32, 3, padding='same', activation='relu'),
+          layers.MaxPooling2D(),
+          layers.Conv2D(64, 3, padding='same', activation='relu'),
+          layers.MaxPooling2D(),
+          layers.Flatten(),
+          layers.Dense(128, activation='relu'),
+          layers.Dense(num_classes)
+        ])
+        self.model.compile(optimizer='adam',
+                      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                      metrics=['accuracy'])
+
+
+    def dataset(self):
+        batch_size = 32
+        img_height = 180
+        img_width = 180
+
+        train_ds = tf.keras.utils.image_dataset_from_directory(
+          data_dir,
+          validation_split=0.2,
+          subset="training",
+          seed=123,
+          image_size=(img_height, img_width),
+          batch_size=batch_size)
+
+        val_ds = tf.keras.utils.image_dataset_from_directory(
+          data_dir,
+          validation_split=0.2,
+          subset="validation",
+          seed=123,
+          image_size=(img_height, img_width),
+          batch_size=batch_size)
+
+        self.class_names = train_ds.class_names
+        self.standardizeData(train_ds)
+
+
+    def standardizeData(self, train_ds):
+        normalization_layer = layers.Rescaling(1./255)
+        normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+        image_batch, labels_batch = next(iter(normalized_ds))
+        first_image = image_batch[0]
+        # Notice the pixel values are now in `[0,1]`.
+        print(np.min(first_image), np.max(first_image))
+
