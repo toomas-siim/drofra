530087048ba6401c14c87a85b6bb3aabde1e675b
toomas-siim <siim.teresk@veebiteenused.ee>
2022-10-08 01:09:23 +0300

    generic fixes

--- system/Script.py
+++ system/Script.py
@@ -24,6 +24,6 @@ class Script:
         scriptNamespace = "scripts." + basename(scriptPath).split(".")[0]
         module = __import__(scriptNamespace)
         class_ = getattr(module, basename(scriptPath).split(".")[0])
-        instance = class_
+        instance = class_()
         instance.init(coreHandle)
         Script.scripts.push(instance)
