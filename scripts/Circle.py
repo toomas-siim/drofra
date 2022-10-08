23e2725b7c3be86a252c270fe5469bdf892a3270
toomas-siim <siim.teresk@veebiteenused.ee>
2022-10-08 11:44:02 +0300

    generic fixes

--- scripts/Circle.py
+++ scripts/Circle.py
@@ -23,7 +23,7 @@ class Circle:
         circlePoint = self.circlePoint
         if circlePoint != None:
             self.rotationPoints = []
-            print(circlePoint)
+            print(circlePoint['lat'])
             self.rotationPoints.append({ lat: circlePoint["lat"] - latLength, lon: circlePoint["lon"] - lonLength })
             self.rotationPoints.append({ lat: circlePoint["lat"] + latLength, lon: circlePoint["lon"] - lonLength })
             self.rotationPoints.append({ lat: circlePoint["lat"] + latLength, lon: circlePoint["lon"] + lonLength })
