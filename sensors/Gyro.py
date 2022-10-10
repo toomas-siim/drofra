5e57b0bdb3fc4e033d1a807c83a62f896b465074
toomas-siim <siim.teresk@veebiteenused.ee>
2022-10-10 18:56:53 +0300

    generic fixes

--- sensors/Gyro.py
+++ sensors/Gyro.py
@@ -17,24 +17,24 @@ class Gyro(Sensor):
 
     def init(self, coreHandle):
         #write to sample rate register
-        bus.write_byte_data(Device_Address, Gyro.SMPLRT_DIV, 7)
+        smbus.write_byte_data(Device_Address, Gyro.SMPLRT_DIV, 7)
 
         #Write to power management register
-        bus.write_byte_data(Device_Address, Gyro.PWR_MGMT_1, 1)
+        smbus.write_byte_data(Device_Address, Gyro.PWR_MGMT_1, 1)
 
         #Write to Configuration register
-        bus.write_byte_data(Device_Address, Gyro.CONFIG, 0)
+        smbus.write_byte_data(Device_Address, Gyro.CONFIG, 0)
 
         #Write to Gyro configuration register
-        bus.write_byte_data(Device_Address, Gyro.GYRO_CONFIG, 24)
+        smbus.write_byte_data(Device_Address, Gyro.GYRO_CONFIG, 24)
 
         #Write to interrupt enable register
-        bus.write_byte_data(Device_Address, Gyro.INT_ENABLE, 1)
+        smbus.write_byte_data(Device_Address, Gyro.INT_ENABLE, 1)
 
     def read_raw_data(addr):
     	#Accelero and Gyro value are 16-bit
-        high = bus.read_byte_data(Device_Address, addr)
-        low = bus.read_byte_data(Device_Address, addr+1)
+        high = smbus.read_byte_data(Device_Address, addr)
+        low = smbus.read_byte_data(Device_Address, addr+1)
 
         #concatenate higher and lower value
         value = ((high << 8) | low)
