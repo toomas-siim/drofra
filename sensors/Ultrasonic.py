2b87daac412cea2f3ad8ae6781289271e3d249e7
toomas-siim <siim.teresk@veebiteenused.ee>
2022-10-10 19:02:12 +0300

    generic fixes

--- sensors/Ultrasonic.py
+++ sensors/Ultrasonic.py
@@ -11,18 +11,19 @@ class Ultrasonic(Sensor):
     def init(self, coreHandle):
         self.writePin = self.getPinByType("write")
         self.readPin = self.getPinByType("read")
+        self.coreHandle = coreHandle
 
     def handle(self):
         if self.waitingForSignal == False and time.time() - self.signalStart >= 1:
             self.waitingForSignal = True
             self.signalStart = time.time()
-            self.pinSystem.setPinType(self.writePin, GPIO.OUT)
-            self.pinSystem.setPinType(self.readPin, GPIO.IN)
-            self.pinSystem.setPinOutput(self.writePin, 1)
+            self.coreHandle.pinSystem.setPinType(self.writePin, GPIO.OUT)
+            self.coreHandle.pinSystem.setPinType(self.readPin, GPIO.IN)
+            self.coreHandle.pinSystem.setPinOutput(self.writePin, 1)
         elif self.waitingForSignal == True:
             if time.time() - self.signalStart >= 0.1:
-                self.pinSystem.setPinOutput(self.writePin, 0)
-            if coreHandle.pinSystem.getPinInput(self.readPin) == 1:
+                self.coreHandle.pinSystem.setPinOutput(self.writePin, 0)
+            if self.coreHandle.pinSystem.getPinInput(self.readPin) == 1:
                 self.waitingForSignal = False
                 self.latestDistance = ((time.time() - self.signalStart) * 34300) / 2
 
